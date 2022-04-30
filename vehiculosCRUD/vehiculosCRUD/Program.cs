cb26c3155ecfd72b283967fb639c9d03e814a21c
sebastianRaiquenParisi <sebastianrparisi@gmail.com>
2022-04-30 20:27:04 -0300

    dbconn string, also missing ; now corrected

--- vehiculosCRUD/vehiculosCRUD/Program.cs
+++ vehiculosCRUD/vehiculosCRUD/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using vehiculosCRUD.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<db_vehiculosContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("dbconn")));
 
 var app = builder.Build();
 
