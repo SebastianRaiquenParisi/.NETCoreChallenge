419b606a68f7c407d2f6d6aaad26849f561a9a73
sebastianRaiquenParisi <sebastianrparisi@gmail.com>
2022-04-30 20:05:55 -0300

    entity models and database done

--- vehiculosCRUD/vehiculosCRUD/Models/Marca.cs
+++ vehiculosCRUD/vehiculosCRUD/Models/Marca.cs
@@ -1,6 +1,18 @@
-﻿namespace vehiculosCRUD.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace vehiculosCRUD.Models
 {
-    public class Marca
+    public partial class Marca
     {
+        public Marca()
+        {
+            Vehiculos = new HashSet<Vehiculo>();
+        }
+
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null!;
+
+        public virtual ICollection<Vehiculo> Vehiculos { get; set; }
     }
 }
--- vehiculosCRUD/vehiculosCRUD/Models/Vehiculo.cs
+++ vehiculosCRUD/vehiculosCRUD/Models/Vehiculo.cs
@@ -1,6 +1,17 @@
-﻿namespace vehiculosCRUD.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace vehiculosCRUD.Models
 {
-    public class Vehiculo
+    public partial class Vehiculo
     {
+        public string Patente { get; set; } = null!;
+        public int IdPropietario { get; set; }
+        public int IdMarca { get; set; }
+        public string Modelo { get; set; } = null!;
+        public int? CantPuertas { get; set; }
+
+        public virtual Marca IdMarcaNavigation { get; set; } = null!;
+        public virtual Propietario IdPropietarioNavigation { get; set; } = null!;
     }
 }
