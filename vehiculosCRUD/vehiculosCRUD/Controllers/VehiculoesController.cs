ee423b20fb9327957d83efcc505f1796d886a7a8
sebastianRaiquenParisi <sebastianrparisi@gmail.com>
2022-05-01 16:38:35 -0300

    auth dependencies installed

---
+++ vehiculosCRUD/vehiculosCRUD/Controllers/VehiculoesController.cs
@@ -0,0 +1,166 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using vehiculosCRUD.Models;
+
+namespace vehiculosCRUD.Controllers
+{
+    public class VehiculoesController : Controller
+    {
+        private readonly db_vehiculosContext _context;
+
+        public VehiculoesController(db_vehiculosContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Vehiculoes
+        public async Task<IActionResult> Index()
+        {
+            var db_vehiculosContext = _context.Vehiculos.Include(v => v.IdMarcaNavigation).Include(v => v.IdPropietarioNavigation);
+            return View(await db_vehiculosContext.ToListAsync());
+        }
+
+        // GET: Vehiculoes/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehiculo = await _context.Vehiculos
+                .Include(v => v.IdMarcaNavigation)
+                .Include(v => v.IdPropietarioNavigation)
+                .FirstOrDefaultAsync(m => m.Patente == id);
+            if (vehiculo == null)
+            {
+                return NotFound();
+            }
+
+            return View(vehiculo);
+        }
+
+        // GET: Vehiculoes/Create
+        public IActionResult Create()
+        {
+            ViewData["IdMarca"] = new SelectList(_context.Marcas, "Id", "Id");
+            ViewData["IdPropietario"] = new SelectList(_context.Propietarios, "Id", "Id");
+            return View();
+        }
+
+        // POST: Vehiculoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Patente,IdPropietario,IdMarca,Modelo,CantPuertas")] Vehiculo vehiculo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(vehiculo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdMarca"] = new SelectList(_context.Marcas, "Id", "Id", vehiculo.IdMarca);
+            ViewData["IdPropietario"] = new SelectList(_context.Propietarios, "Id", "Id", vehiculo.IdPropietario);
+            return View(vehiculo);
+        }
+
+        // GET: Vehiculoes/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehiculo = await _context.Vehiculos.FindAsync(id);
+            if (vehiculo == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdMarca"] = new SelectList(_context.Marcas, "Id", "Id", vehiculo.IdMarca);
+            ViewData["IdPropietario"] = new SelectList(_context.Propietarios, "Id", "Id", vehiculo.IdPropietario);
+            return View(vehiculo);
+        }
+
+        // POST: Vehiculoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Patente,IdPropietario,IdMarca,Modelo,CantPuertas")] Vehiculo vehiculo)
+        {
+            if (id != vehiculo.Patente)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(vehiculo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VehiculoExists(vehiculo.Patente))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdMarca"] = new SelectList(_context.Marcas, "Id", "Id", vehiculo.IdMarca);
+            ViewData["IdPropietario"] = new SelectList(_context.Propietarios, "Id", "Id", vehiculo.IdPropietario);
+            return View(vehiculo);
+        }
+
+        // GET: Vehiculoes/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehiculo = await _context.Vehiculos
+                .Include(v => v.IdMarcaNavigation)
+                .Include(v => v.IdPropietarioNavigation)
+                .FirstOrDefaultAsync(m => m.Patente == id);
+            if (vehiculo == null)
+            {
+                return NotFound();
+            }
+
+            return View(vehiculo);
+        }
+
+        // POST: Vehiculoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var vehiculo = await _context.Vehiculos.FindAsync(id);
+            _context.Vehiculos.Remove(vehiculo);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VehiculoExists(string id)
+        {
+            return _context.Vehiculos.Any(e => e.Patente == id);
+        }
+    }
+}
